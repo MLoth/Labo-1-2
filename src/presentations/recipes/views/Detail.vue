09e9599a81fa3eb345607015f1017a9ff10d81f0
Martijn Loth <MLoth@users.noreply.github.com>
2020-09-30 10:32:43 +0200

    Fixed rerender of detail page

--- src/presentations/recipes/views/Detail.vue
+++ src/presentations/recipes/views/Detail.vue
@@ -2,7 +2,9 @@
   <div class="py-6 px-16">
     <div class="flex justify-between">
       <div class="flex items-center">
-        <h1 class="text-4xl font-semibold text-gray-900">Tomato soup</h1>
+        <h1 v-if="state.recipe" class="text-4xl font-semibold text-gray-900">
+          {{ state.recipe.recipeName }}
+        </h1>
         <button
           class="opacity-50 hover:opacity-100 ml-3 focus:outline-none text-xs text-orange-800"
         >
@@ -34,23 +36,23 @@
       </div>
     </div>
 
-    <div class="flex text-sm my-10">
+    <div v-if="state.recipe" class="flex text-sm my-10">
       <div class="w-32">
         <p class=" opacity-50">Category</p>
-        <p>Soup</p>
+        <p>{{ state.recipe.categoryName }}</p>
       </div>
       <div class="w-32">
         <p class=" opacity-50">Vegetarion</p>
-        <p>Yes</p>
+        <p>{{ state.recipe.vegetarian }}</p>
       </div>
       <div class="w-32">
         <p class=" opacity-50">Time</p>
-        <p>45m</p>
+        <p>{{ state.recipe.totalSecondsToPrepare }}m</p>
       </div>
     </div>
 
-    <p class="mt-4">
-      A delicious tomatosoup 🥫.
+    <p v-if="state.recipe" class="mt-4">
+      {{ state.recipe.recipeDescription }}
     </p>
 
     <button class="mt-4 text-orange-600">
@@ -61,10 +63,36 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { get } from '@/utils/api';
+import { reactive } from 'vue';
 
 export default defineComponent({
   setup() {
-    return {};
+    const state = reactive({
+      recipe: null,
+    });
+    return {
+      state,
+    };
+  },
+
+  async created() {
+    const id = this.$route.params.id;
+    this.getRecipe(id.toString());
+  },
+
+  watch: {
+    $route(to, from) {
+      this.getRecipe(to.params.id);
+    },
+  },
+
+  methods: {
+    async getRecipe(id: string) {
+      const data = await get(`Recipes/${id}`);
+      // this.state.recipe = data.find((recipe: any) => recipe.id == id); // Zelf de correcte er uit halen.
+      this.state.recipe = data; // Zelf de correcte er uit halen.
+    },
   },
 });
 </script>
